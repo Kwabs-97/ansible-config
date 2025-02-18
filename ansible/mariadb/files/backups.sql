1fff3b9055d1ccaef3a5ca6d3ba39bf2d9d7464c
Samuel <khobewayne@gmail.com>
2025-02-18 19:54:01 +0000

    database backup start

---
+++ ansible/mariadb/files/backups.sql
@@ -0,0 +1,8 @@
+create table if not exists list (
+    id          int(11)  unsigned auto_increment primary key,    
+    date        datetime                         null,
+    target      varchar(100) charset utf8mb4     null,
+    start_time  time                             null,
+    end_time    time                             null,
+    status      varchar(100)                     null,
+)
